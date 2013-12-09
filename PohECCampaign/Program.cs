8179a3b33ef395d9e11b52ef14ff15c8a0270d95
yoshikazuendo <ring2_bell@hotmail.com>
2013-12-09 16:59:53 +0900

    機能を実装した。
    とりあえず何も考えずに。
    これだと。テストケース１だけ通る。テストケース２以降はタイムオーバー。

--- PohECCampaign/Program.cs
+++ PohECCampaign/Program.cs
@@ -10,7 +10,33 @@ namespace PohECCampaign
 	{
 		static void Main(string[] args)
 		{
+			var counts = Console.ReadLine().Trim().Split(' ');
+			int itemCount = int.Parse(counts[0]);
+			var itemPrices = new int[itemCount];
+			for (int i = 0; i < itemCount; i++) {
+				itemPrices[i] = int.Parse(Console.ReadLine().Trim());
+			}
 
+			int campaignDay = int.Parse(counts[1]);
+			var campaignPrices = new int[campaignDay];
+			for (int i = 0; i < campaignDay; i++) {
+				campaignPrices[i] = int.Parse(Console.ReadLine().Trim());
+			}
+
+			foreach (var campaignPrice in campaignPrices) {
+				int bestPrice = 0;
+
+				for (int i = 0; i < itemCount; i++) {
+					for (int j = i + 1; j < itemCount; j++) {
+						int totalPrice = itemPrices[i] + itemPrices[j];
+						if (bestPrice <= totalPrice && totalPrice <= campaignPrice) {
+							bestPrice = totalPrice;
+						}
+					}
+				}
+
+				Console.WriteLine(bestPrice);
+			}
 		}
 	}
 }
